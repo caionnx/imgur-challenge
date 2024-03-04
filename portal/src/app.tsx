55132d5a31cda188b4da72643228fa4cb72ccde9
Caio Nunes <caionavarro.n@hotmail.com>
2024-03-04 14:17:49 +0000

    feat(portal): add tailwind for styling

--- portal/src/app.tsx
+++ portal/src/app.tsx
@@ -1,10 +1,9 @@
 const App = ({ initialState }: { initialState: ImgurRestApi.GalleryItem[] | undefined }) => {
   return (
     <main>
-      <h1>Portal</h1>
+      <h1 className="text-3xl font-bold underline">Portal</h1>
       <p>Welcome to the best of Imgur!</p>
       <div>
-        <span></span>
         <button>Button</button>
       </div>
       <pre>{JSON.stringify(initialState, null, 2)}</pre>
