3c5460a97a66594bd131eae8c7fc868da762318d
Caio Nunes <caionavarro.n@hotmail.com>
2024-03-05 14:31:39 +0000

    feat(portal): add prettier config

---
+++ portal/.prettier.config.js
@@ -0,0 +1,28 @@
+import tailwindcss from 'prettier-plugin-tailwindcss';
+
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxBracketSameLine: false,
+  endOfLine: 'lf',
+  tailwindConfig: './tailwind.config.js',
+  overrides: [
+    {
+      files: '.prettierrc',
+      options: {
+        parser: 'json',
+      },
+    },
+  ],
+  plugins: [tailwindcss],
+};
+
+export default config;
