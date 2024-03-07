a105dbff76ee7380b75e62a16ba8cafc2b180d5e
Caio Nunes <caionavarro.n@hotmail.com>
2024-03-07 10:09:44 +0000

    fix(portal): fix thumbnail test with missing props

--- portal/src/components/Thumbnail/tests/Thumbnail.test.tsx
+++ portal/src/components/Thumbnail/tests/Thumbnail.test.tsx
@@ -42,7 +42,7 @@ describe("Thumbnail", () => {
   it("should render image with title", () => {
     const titleText = "My Test";
 
-    render(<Thumbnail title={titleText} media={testMedia} />);
+    render(<Thumbnail id="4597" onClick={() => undefined} title={titleText} media={testMedia} />);
 
     const title = screen.getByText(titleText);
     const image = screen.getByAltText(titleText);
@@ -59,7 +59,7 @@ describe("Thumbnail", () => {
       mp4: "linktovideo",
     };
 
-    render(<Thumbnail title={titleText} media={videoMedia} />);
+    render(<Thumbnail id="4597" onClick={() => undefined} title={titleText} media={videoMedia} />);
     mockAllIsIntersecting(true);
 
     const title = screen.getByText(titleText);
