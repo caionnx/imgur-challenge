79c966405edd7187ad725b7d4604ea441b6edf86
Caio Nunes <caionavarro.n@hotmail.com>
2024-03-07 11:57:34 +0000

    fix(portal): Fix queryclient creation to avoid cache issues between different renders

--- portal/src/index.tsx
+++ portal/src/index.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Home } from "./pages/index";
 import { GalleryProvider } from "./providers/GalleryProvider";
-
-const queryClient = new QueryClient();
 
 const App = ({
   initialState,
 }: {
   initialState: ImgurRestApi.GalleryAlbum[] | undefined;
 }) => {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
       <GalleryProvider initialState={initialState}>
